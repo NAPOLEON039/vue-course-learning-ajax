2b07c869c07b2f91b9f4296e2d99fb01b3bf5109
NAPOLEON039 <nihar.raote@gmail.com>
2018-08-30 18:31:56 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8 offset-sm-2">
+        <h1>HTTP</h1>
+        <div class="form-group">
+          <label class="col-form-label" for="username">Username</label>
+          <input class="form-control" type="text" name="username" id="username" v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label class="col-form-control" for="mail">Email</label>
+          <input class="form-control" type="email" name="email" id="mail" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.user);
+    }
+  }
+}
+</script>
